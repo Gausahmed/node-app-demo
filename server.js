bb4d08ce1c2941d00f96cd558df52d5a91007dc6
Gaus Mohiuddin Ahmed <74852695+Gausahmed@users.noreply.github.com>
2022-08-03 16:50:28 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+'use strict';
+const express = require('express');
+const PORT = 8080
+const HOST = '0.0.0.0';
+
+const app = express();
+app.get('/', (req, res) => {
+    res.send('<h1>Gaus ahmed</h1>');
+});
+
+app.listen(PORT,HOST);
+console.log('Running on http://&{HOST}:${PORT}');
